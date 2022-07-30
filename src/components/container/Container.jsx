2125d8b67ed3451f48abaf851f7770538a9da1ed
Napatsakorn Pianchana <napatdmsu@gmail.com>
2022-07-30 18:22:23 +0700

    Modifiy header and container

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,8 +1,170 @@
 import React from "react";
 import "./container.css";
 
-function Container() {
-  return <div>Container</div>;
+const Container = () => {
+  return (
+
+    <div className="mb-14">
+      <div className="flex justify-around mt-14" >
+        <a href="#" className="block p-6 max-w-sm bg-white rounded-[40px] border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">image</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Topic</h5>
+          </div>
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Datetime</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Duration</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Type</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Description</h5>
+          </div>
+
+          <div className="grid grid-cols-3 gap-36">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Status</h5>
+            <div className="grid grid-cols-2 gap-24">
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Edit</h5>
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Delete</h5>
+            </div>
+
+          </div>
+        </a>
+        <a href="#" className="block p-6 max-w-sm bg-white rounded-[40px] border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">image</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Topic</h5>
+          </div>
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Datetime</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Duration</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Type</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Description</h5>
+          </div>
+
+          <div className="grid grid-cols-3 gap-36">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Status</h5>
+            <div className="grid grid-cols-2 gap-24">
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Edit</h5>
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Delete</h5>
+            </div>
+
+          </div>
+        </a>
+        <a href="#" className="block p-6 max-w-sm bg-white rounded-[40px] border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">image</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Topic</h5>
+          </div>
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Datetime</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Duration</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Type</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Description</h5>
+          </div>
+
+          <div className="grid grid-cols-3 gap-36">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Status</h5>
+            <div className="grid grid-cols-2 gap-24">
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Edit</h5>
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Delete</h5>
+            </div>
+
+          </div>
+        </a>
+      </div>
+      <div className="flex justify-around mt-11" >
+        <a href="#" className="block p-6 max-w-sm bg-white rounded-[40px] border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">image</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Topic</h5>
+          </div>
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Datetime</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Duration</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Type</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Description</h5>
+          </div>
+
+          <div className="grid grid-cols-3 gap-36">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Status</h5>
+            <div className="grid grid-cols-2 gap-24">
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Edit</h5>
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Delete</h5>
+            </div>
+
+          </div>
+        </a>
+        <a href="#" className="block p-6 max-w-sm bg-white rounded-[40px] border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">image</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Topic</h5>
+          </div>
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Datetime</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Duration</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Type</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Description</h5>
+          </div>
+          
+          <div className="grid grid-cols-3 gap-36">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Status</h5>
+            <div className="grid grid-cols-2 gap-24">
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Edit</h5>
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Delete</h5>
+            </div>
+
+          </div>
+        </a>
+        <a href="#" className="block p-6 max-w-sm bg-white rounded-[40px] border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">image</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Topic</h5>
+          </div>
+          <div className="grid grid-cols-2">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Datetime</h5>
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Duration</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Type</h5>
+          </div>
+          <div className="grid">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Description</h5>
+          </div>
+
+          <div className="grid grid-cols-3 gap-36">
+            <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Status</h5>
+            <div className="grid grid-cols-2 gap-24">
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Edit</h5>
+              <h5 className="mb-2 text-lg tracking-tight text-gray-900 dark:text-white sm:flex-wrap">Delete</h5>
+            </div>
+
+          </div>
+        </a>
+      </div>
+
+
+    </div>
+
+  )
+
 }
 
 export default Container;
