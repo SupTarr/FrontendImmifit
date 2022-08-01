5d4a644ca1d1d4e3870e00dcb17e2548717b3afb
Suwijak Lohanan <suwijaklohanan@hotmail.com>
2022-08-02 00:57:18 +0700

    add back button

---
+++ src/components/form-component-header/header-form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header-form.css";
+
+function HeaderForm() {
+  return(
+    <div className="form-header">
+        <a href="#" class="previous">&laquo; Home</a>
+    </div>
+  )
+}
+
+export default HeaderForm;
