1ec77c4186b4bd0baf5dcf10d5175cf1125b01e6
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-08-02 19:14:16 +0700

    Add Responsive of Header + Add link to form

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,36 +1,28 @@
 import React from "react";
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'  
 import "./header.css";
 
 const Header = () => {
-
   return (
-    <div className="Header">
+    <div id="Header-desktop" className="Header">
       <div className="Container">
-        <div className="Button">
-          ALL
-        </div>
-        <div className="Button">
-          RUN
-        </div>
-        <div className="Button">
-          SWIM
-        </div>
-        <div className="Button">
+        <div className="Button">ALL</div>
+        <div className="Button">RUN</div>
+        <div className="Button">SWIM</div>
+        <select className="dropDown">
           <option value="BIKE">BIKE</option>
           <option value="WALK">WALK</option>
           <option value="STACK">STACK</option>
           <option value="HOCKEY">HOCKEY</option>
-        </div>
+        </select>
       </div>
       <div className="Activity">
-        <div className="Button-Activity">
-          Add Activity
-        </div>
+        <button className="Button-Activity">
+          <Link to="/form/1">Add Activity</Link>
+        </button>
       </div>
     </div>
-
-
-  )
-}
+  );
+};
 
 export default Header;
