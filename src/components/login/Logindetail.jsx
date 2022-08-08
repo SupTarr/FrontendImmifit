7bd98b06d21848712ce52527a120a9c0ebe8e2ea
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-08-08 20:48:27 +0700

    Mobile

--- src/components/login/Logindetail.jsx
+++ src/components/login/Logindetail.jsx
@@ -7,13 +7,13 @@ import Tel from './telephone.png'
 
 const Logindetail = () => {
   return (
-    <div className='login h-screen relative'>
+    <div className='login relative sm:h-screen'>
       {/* <button className='w-[50px]'>
           <img src={Back} alt="" />
         </button> */}
-      <div className='flex flex-col justify-center items-center pt-16'>
-        <div className="max-w-5xl flex flex-col justify-center sm:flex-row">
-            <img className="shadow-md sm:rounded-l-xl object-cover mobile:mx-auto mobile:w-[50%] sm:max-w-[200px] md:max-w-sm lg:max-w-lg xl:max-w-xl" src="../../public/imagecard2.jpg" alt="imagecard"></img>
+      <div className='flex flex-col justify-center items-center sm:pt-16 h-[100%]'>
+        <div className="max-w-5xl flex flex-col justify-center sm:flex-row mobile:m-10">
+            <img className="shadow-md sm:rounded-l-xl object-cover  sm:max-w-[200px] md:max-w-sm lg:max-w-lg xl:max-w-xl" src="../../public/imagecard2.jpg" alt="imagecard"></img>
           <form className="flex flex-col justify-center bg-white shadow-md sm:rounded-r-xl px-5 md:max-w-[250px] xl:max-w-[350px] py-5">
             <div className="flex text-gray-700 text-2xl font-bold mb-10">
               Log in
