c5eba8101eaa855b1604d8ea325da4a5cb846794
Napatsakorn Pianchana <napatdmsu@gmail.com>
2022-08-09 11:34:12 +0700

    add style Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,19 +5,19 @@ const Header = () => {
 
   return (
     <div className="Header">
-      <div className="Container bg-white bg-opacity-50 backdrop-blur-xl rounded-full drop-shadow-lg">
-        <div className="Button hover:bg-[#005B97] hover:text-white py-2 px-4 bg-white bg-opacity-50 backdrop-blur-xl rounded-full drop-shadow-lg font-semibold">
+      <div className="Container bg-white bg-opacity-50 backdrop-blur-sm rounded-full drop-shadow-lg">
+        <div className="Button hover:bg-[#005B97] hover:text-white py-2 px-4 bg-white bg-opacity-50 backdrop-blur-sm rounded-full drop-shadow-lg font-semibold">
           ALL
         </div>
-        <div className="Button hover:bg-[#005B97] hover:text-white py-2 px-4 bg-white bg-opacity-50 backdrop-blur-xl rounded-full drop-shadow-lg font-semibold">
+        <div className="Button hover:bg-[#005B97] hover:text-white py-2 px-4 bg-white bg-opacity-50 backdrop-blur-sm rounded-full drop-shadow-lg font-semibold">
           RUN
         </div>
-        <div className="Button hover:bg-[#005B97] hover:text-white py-2 px-4 bg-white bg-opacity-50 backdrop-blur-xl rounded-full drop-shadow-lg font-semibold">
+        <div className="Button hover:bg-[#005B97] hover:text-white py-2 px-4 bg-white bg-opacity-50 backdrop-blur-sm rounded-full drop-shadow-lg font-semibold">
           SWIM
         </div>
         <div className="Button rounded-full">
           <div className="dropdown inline-block relative">
-            <button className="hover:bg-[#005B97] hover:text-white bg-white bg-opacity-50 backdrop-blur-xl drop-shadow-lg text-black font-semibold py-2 px-4 rounded-full inline-flex items-center">
+            <button className="hover:bg-[#005B97] hover:text-white bg-white bg-opacity-50 backdrop-blur-sm drop-shadow-lg text-black font-semibold py-2 px-4 rounded-full inline-flex items-center">
               <span className="mr-1">Bike</span>
               <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
             </button>
@@ -31,7 +31,7 @@ const Header = () => {
         </div>
       </div>
       <div className="Activity">
-        <div className="Button-Activity mr-10 hover:bg-[#005B97] hover:text-white text-black font-semibold bg-white bg-opacity-50 backdrop-blur-xl rounded-ful drop-shadow-lg">
+        <div className="Button-Activity mr-10 hover:bg-[#005B97] hover:text-white text-black font-semibold bg-white bg-opacity-50 backdrop-blur-sm rounded-ful drop-shadow-lg">
           Add Activity
         </div>
       </div>
