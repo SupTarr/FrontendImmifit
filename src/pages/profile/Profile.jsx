7b7a3e87523b6c1b29a55c33b83f40c1ecbd7438
Napatsakorn Pianchana <napatdmsu@gmail.com>
2022-08-17 00:12:34 +0700

    add shadow button

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -9,9 +9,10 @@ function Profile() {
       </div>
       <div className="pt-10">
         <p className="text-center pb-10">Login first time? <a className="font-bold">Create Profile</a></p>
-        <button className="mx-auto bg-[#F08080] hover:bg-[#ff5757] text-white px-10 py-2 rounded-full flex justify-center">
+        <button className="mx-auto bg-[#F08080] hover:bg-[#ff5757] shadow-md hover:shadow-lg text-white font-bold px-10 py-2 rounded-full flex justify-center">
           Create Profile
         </button>
+        
       </div>
     </div>
   );
