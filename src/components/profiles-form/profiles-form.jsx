ca2e6a4fd9aa4f41b292a2426ced0ad11f652aab
Suwijak Lohanan <suwijaklohanan@hotmail.com>
2022-08-18 02:31:29 +0700

    form pre stylling

--- src/components/profiles-form/profiles-form.jsx
+++ src/components/profiles-form/profiles-form.jsx
@@ -48,9 +48,148 @@ function Profileform() {
 						</p>
 					</div>
 
-          <div>
-            <label></label>
-          </div>
+					<div>
+						<label
+							className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+							for="file_input"
+						>
+							Profile Name
+						</label>
+						<input
+							type="text"
+							name="first-name"
+							id="first-name"
+							autoComplete="given-name"
+							className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+						/>
+					</div>
+
+					<div>
+						<label
+							htmlFor="about"
+							className="block text-sm font-medium text-gray-700"
+						>
+							About
+						</label>
+						<div className="mt-1">
+							<textarea
+								id="about"
+								name="about"
+								rows={3}
+								className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+								placeholder="About you"
+								defaultValue={""}
+							/>
+						</div>
+						<p className="mt-2 text-sm text-gray-500">
+							Brief description for your profile. URLs are hyperlinked.
+						</p>
+					</div>
+
+					<div className="col-span-6 sm:col-span-3">
+						<label
+							htmlFor="country"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Gender
+						</label>
+						<select
+							id="gender"
+							name="gender"
+							autoComplete="gender"
+							className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+						>
+							<option>Male</option>
+							<option>Female</option>
+							<option>LBGT+</option>
+						</select>
+					</div>
+
+					<div>
+						<label
+							className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+							for="file_input"
+						>
+							Age
+						</label>
+						<input
+							type="text"
+							name="age"
+							id="age"
+							autoComplete="your_age"
+							className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+						/>
+					</div>
+
+
+					<div className="shadow overflow-hidden sm:rounded-md">
+						<div className="px-4 py-5 sm:p-6">
+							<div className="grid grid-cols-6 gap-6">
+								<div className="col-span-6 sm:col-span-3">
+									<label
+										htmlFor="height"
+										className="block text-sm font-medium text-gray-700"
+									>
+										Height
+									</label>
+									<input
+										type="text"
+										name="height"
+										id="height"
+										autoComplete="your_height"
+										className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+									/>
+								</div>
+
+								<div className="col-span-6 sm:col-span-3">
+									<label
+										htmlFor="weight"
+										className="block text-sm font-medium text-gray-700"
+									>
+										Weight
+									</label>
+									<input
+										type="text"
+										name="weight"
+										id="weight"
+										autoComplete="your_weight"
+										className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div>
+						<label
+							className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+							for="bmi"
+						>
+							BMI
+						</label>
+						<input
+							type="text"
+							name="bmi"
+							id="bmi"
+							autoComplete="bmi"
+							className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+						/>
+					</div>
+
+          <div className="px-4 py-3 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="reset"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Reset
+                  </button>
+                </div>
 				</form>
 			</div>
 		</div>
