648c6272006cb1b80f893d0d80f674cdf91776e7
Suwijak Lohanan <suwijaklohanan@hotmail.com>
2022-08-18 16:11:14 +0700

    fix url

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Route path="/register/" element={<Register />} />
         <Route path="/password_reset/" element={<Passreset />} />
         <Route path="/forget_password/" element={<Forgotpassword/>} />
-        <Route path="/form_proflie/" element={<Profilesform/>} />
+        <Route path="/form_profile/" element={<Profilesform/>} />
         {/* <Route path="/form/:id" element={<Form />} />
         <Route path="/login/:id" element={<Login />} />
         <Route path="/register/:id" element={<Register />} /> */}
