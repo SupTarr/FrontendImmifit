13e80e9fad4df408a8bf69f92d9985da25f0532f
Napatsakorn Pianchana <napatdmsu@gmail.com>
2022-08-24 15:21:50 +0700

    debug delete

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import axios from "../../api/axios";
 import './card.css'
 import moment from 'moment';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Header from '../header/Header';
+// Context
+// import { createContext, useContext} from 'react';
+
 
 
 const config = {
@@ -13,30 +17,43 @@ const config = {
   }
 }
 
-const Card = ({ user, users, setUsers }) => {
-  // const title_name = user.start
-  var now = new Date(user.end_time);
-  var then = new Date(user.start_time);
-  var datetime = new Date(user.date);
+
+// const NewActContext = React.createContext();
+
+
+
+const Card = ({
+  item,
+  setAllUsers,
+  allUsers,
+  getUsers }) => {
+  // const [newActivity, setNewActivity] = useState([]);
+
+  // const title_name = item.start
+  var now = new Date(item.end_time);
+  var then = new Date(item.start_time);
+  var datetime = new Date(item.date);
   var date = moment(datetime).format('DD/MM/YYYY');
   // console.log(datetime)
-  // console.log(user.activity_id)
+  // console.log(item.activity_id)
   
-  console.log(user)
+  console.log(item)
   var duration = (now - then) / 60000;
   // console.log(duration)
 
- 
+  
   const handleDeleteClick  = async (id) => {
     try {
       // e.preventDefault();
-      // console.log(id)
-      const id = user.activity_id;
-      await axios.delete(`/activities/${user.activity_id}`, config)      
-      const newActivity = users.filter((user) => user.activity_id !== id);
-      setUsers(newActivity); 
+      const id = item.activity_id;
+      await axios.delete(`/activities/${item.activity_id}`, config)      
+      const newActivity = allUsers.filter((item) => item.activity_id !== id);
+      // console.log(setAllUsers)
+      setAllUsers(newActivity); 
+      // setNewActivity(newActivity); 
       
-      console.log(user.activity_id)
+      console.log(id)
+      // console.log(newActivity)
       
     } catch (error) {
       console.log(error)
@@ -46,17 +63,17 @@ const Card = ({ user, users, setUsers }) => {
   
 
   return (
+    // <NewActContext value={{newActivity, setNewActivity}}>
     <div>
-
       <div className="flex justify-around mx-auto" >
         <figure className="snip1174 hover:bg-white rounded-[40px]">
-          <img src={user.img.url} alt="imgcard" className='block rounded-[40px] border shadow-md hover:bg-white dark:bg-gray-800 dark:border-gray-700  dark:hover:bg-gray-700 sm:mx-auto ' />
+          <img src={item.img.url} alt="imgcard" className='block rounded-[40px] border shadow-md hover:bg-white dark:bg-gray-800 dark:border-gray-700  dark:hover:bg-gray-700 sm:mx-auto ' />
           <figcaption>
             <div className="grid grid-cols-5 ">
               <h5 className="mb-2 text-sm tracking-tight text-black dark:text-white col-span-1">
 
               </h5>
-              <h5 className="col-span-4 mb-2 text-sm tracking-tight text-black dark:text-white">{user.title}</h5>
+              <h5 className="col-span-4 mb-2 text-sm tracking-tight text-black dark:text-white">{item.title}</h5>
             </div>
             <div className="grid grid-cols-2">
               <h5 className="mb-2 text-sm tracking-tight text-black dark:text-white">Date: {date}</h5>
@@ -66,12 +83,12 @@ const Card = ({ user, users, setUsers }) => {
             </div>
             <div className="grid">
               <h5 className="mb-2 text-sm tracking-tight text-black dark:text-white">
-                Type: {user.activity_type}
+                Type: {item.activity_type}
               </h5>
             </div>
             <div className="grid">
               <h5 className="mb-2 text-sm tracking-tight text-black dark:text-white">
-                Description: {user.description}
+                Description: {item.description}
               </h5>
             </div>
 
@@ -88,7 +105,9 @@ const Card = ({ user, users, setUsers }) => {
                 <button href="/form/" className="bg-[#F08080] hover:bg-[#ff5757] text-white font-bold px-10 py-2 shadow-md hover:shadow-lg rounded flex justify-center">
                   Edit
                 </button>
-                <button className="bg-[#F08080] hover:bg-[#ff5757] text-white font-bold px-10 py-2 shadow-md hover:shadow-lg rounded flex justify-center" onClick={handleDeleteClick}>
+                <button className="
+                bg-[#F08080] hover:bg-[#ff5757] text-white font-bold px-10 py-2 
+                shadow-md hover:shadow-lg rounded flex justify-center" onClick={handleDeleteClick} getUsers={getUsers} >
                   Delete
                 </button>
               </div>
@@ -98,8 +117,15 @@ const Card = ({ user, users, setUsers }) => {
 
       </div>
     </div>
+    // {children}
+    // </NewActContext>
   )
-}
+};
 
+
+
+// export {NewActContext}
 export default Card
+
+
 
