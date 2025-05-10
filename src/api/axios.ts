b19e58bcff7ec2d2bd0a0c43d46d2a7c6acc976d
SupTarr <tatasuppakrit@gmail.com>
2025-05-10 22:40:50 +0700

    feat: add new backend path

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,12 +1,10 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
-const BASE_URL: string = "https://immifit-backend.vercel.app/";
+const BASE_URL: string = "https://immifit-backend.suptarr.vercel.app/";
 
 const defaultConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: {
-    "Access-Control-Allow-Origin":
-      "http://127.0.0.1:5173,https://immifit.vercel.app/",
     "Content-Type": "application/json",
   },
 };
